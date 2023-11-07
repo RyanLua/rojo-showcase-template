902f031e0e47cf36ac7cc097c95cc10888f5539d
Ryan Luu <ryan.luu@ryanluu.dev>
2023-11-06 21:49:33 -0800

    Update Admins.lua with new GroupId
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- src/ServerScriptService/AdminCommands/Admins.lua
+++ src/ServerScriptService/AdminCommands/Admins.lua
@@ -1,6 +1,6 @@
 local Admins = {
 	-- Users in this group will have their permission level set to their current rank in the group
-	GroupId = -1,
+	GroupId = 4973811,
 	-- Ranks set here for player UserIds will override ranks from the GroupId
 	UserIds = {
 		-- [game.CreatorId] = 255,
