f7884990af6005fb86bde01ffaac3c9a9ec17fcf
Ryan Luu <ryan.luu@ryanluu.dev>
2024-04-29 20:40:00 -0700

    Fix state errors and hide gui
    Signed-off-by: Ryan Luu <ryan.luu@ryanluu.dev>

--- src/client/ScreenshotHud.client.lua
+++ src/client/ScreenshotHud.client.lua
@@ -8,6 +8,8 @@ local Icon = require(ReplicatedStorage.Icon)
 
 local screenshotHud = GuiService:WaitForChild("ScreenshotHud") :: ScreenshotHud
 screenshotHud.ExperienceNameOverlayEnabled = true
+screenshotHud.HideCoreGuiForCaptures = true
+screenshotHud.HidePlayerGuiForCaptures = true
 screenshotHud.OverlayFont = Enum.Font.GothamMedium
 
 -- Create the topbar icon
@@ -18,11 +20,20 @@ icon:setImageScale(0.7)
 
 -- Change the ScreenshotHud and CoreGuiEnabled when icon is toggled
 icon.toggled:Connect(function(isSelected: boolean): ()
-    StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, isSelected)
-    screenshotHud.Visible = isSelected
+	StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.All, not isSelected)
+	screenshotHud.Visible = isSelected
 end)
 
 -- Close the ScreenshotHud when the Roblox coregui escape menu is opened
 GuiService.MenuOpened:Connect(function(): ()
 	icon:deselect()
+end)
+
+-- Deselect the icon if ScreenshotHud is closed or opened
+screenshotHud:GetPropertyChangedSignal("Visible"):Connect(function(): ()
+	if screenshotHud.Visible == false then
+		icon:deselect()
+	elseif screenshotHud.Visible == true then
+		icon:select()
+	end
 end)
